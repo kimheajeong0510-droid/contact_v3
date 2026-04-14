b4f6f97848752be343e78b7eabba1e9024ee4d69
kimheajeong <kimheajeong0510@gmail.com>
2026-04-14 14:23:37 +0900

    #2. 기본 view 생성 및 ContactVO Class 생성

---
+++ src/vo/Contact.java
@@ -0,0 +1,41 @@
+package vo;
+public class Contact {
+    private long id;
+    private String name;
+    private int age;
+    private String phone;
+    // 생성자
+    public Contact(long id, String name, int age, String phone) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+    }
+    // Getter
+    public long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    // Setter
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    @Override
+    public String toString(){
+        return "[" + id + "]" + name + " / " + age + " / " + phone;
+    }
+}
